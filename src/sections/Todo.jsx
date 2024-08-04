d8388b2f288e302ebac77cb5009007257726ed0b
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-08-04 22:38:07 +0700

    Add files via upload

--- src/sections/Todo.jsx
+++ src/sections/Todo.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import "./static/Todo.css"
 const Todo = () => {
   return (
-    <div>
-      <h1>Todo</h1>
-    </div>
+    <>
+      <div class = "content" id = "todo-tab">
+            <div class = "todo-frame">
+                <input type="text" id = "task-input" placeholder="Enter a task: "/>
+                <button id = "task-button" onclick="add_task()">Add</button>
+                <p id="no_of_task">Number of tasks: <div id = "task-count" onchange="incrementTask()">0</div></p>
+            </div> 
+
+            <div class = "list-frame">
+                <ul id = "todo-list"></ul>
+            </div>
+        
+        </div>
+    </>
   );
 };
 
